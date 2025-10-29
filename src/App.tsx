9f4caca936711db035a656d7f30d2afb734c02fb
Dan <dandara.oliveira.n@gmail.com>
2025-10-28 21:15:41 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import FormTema from './components/tema/formtema/FormTema'
+import ListaTemas from './components/tema/listatemas/ListaTemas'
 import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import DeletarTema from './components/tema/deletartema/DeletarTema'
 
 function App() {
 	return (
@@ -18,6 +21,11 @@ function App() {
 					<Route path='/' element={<Login />} />
 					<Route path='/home' element={<Home />} />
 					<Route path='/cadastro' element={<Cadastro />} />
+					<Route path='/login' element={<Login />} />
+					<Route path="/temas" element={<ListaTemas />} />
+					<Route path="/cadastrartema" element={<FormTema />} />
+					<Route path="/editartema/:id" element={<FormTema />} />
+					<Route path="/deletartema/:id" element={<DeletarTema />} />
 				</Routes>
 			</div>
 				
@@ -26,7 +34,7 @@ function App() {
 			</BrowserRouter>
 			</AuthProvider>
 		</>
-	)
+	);
 }
 
-export default App
+export default App;
